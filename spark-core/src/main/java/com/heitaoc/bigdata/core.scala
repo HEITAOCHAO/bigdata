2af743d510b5b6c08bb5d15492a8d81159d2697d
HEITAOCHAO <1185597412@qq.ocm>
2021-04-28 22:31:31 +0800

    项目搭建

---
+++ spark-core/src/main/java/com/heitaoc/bigdata/core.scala
@@ -0,0 +1,13 @@
+package com.heitaoc.bigdata
+
+/**
+ * @Author: 郭超
+ * @DateTime: 2021/4/28 22:10
+ */
+object core {
+
+  def main(args: Array[String]): Unit = {
+    println("hello spark!")
+  }
+
+}
